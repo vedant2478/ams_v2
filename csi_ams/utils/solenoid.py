15ce7794c0808c33967690dc118f38ebc0601775
vedant2478 <vedant@csinc.in>
2025-12-29 17:44:48 +0530

    Updated key status when removed

--- csi_ams/utils/solenoid.py
+++ csi_ams/utils/solenoid.py
@@ -8,8 +8,10 @@ SOL.dir(mraa.DIR_OUT)
 
 try:
     while True:
+        print("Activating solenoid...")
         SOL.write(1)
         time.sleep(2)
+        print("Deactivating solenoid...")
         SOL.write(0)
         time.sleep(2)
 except KeyboardInterrupt:
