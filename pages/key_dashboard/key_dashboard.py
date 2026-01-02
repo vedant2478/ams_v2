dc8517be9c4b808bb6c475828cd38a8dda079d35
vedant2478 <vedant@csinc.in>
2026-01-02 11:56:44 +0530

    Log events

--- pages/key_dashboard/key_dashboard.py
+++ pages/key_dashboard/key_dashboard.py
@@ -11,7 +11,7 @@ from db import get_keys_for_activity, set_key_status_by_peg_id
 from test import AMS_CAN
 
 # 🔒 DOOR LIMIT SWITCH (SINGLE SOURCE OF TRUTH)
-from csi_ams.utils.commons import read_limit_switch, LIMIT_SWITCH
+# from csi_ams.utils.commons import read_limit_switch, LIMIT_SWITCH
 
 
 # =========================================================
@@ -126,30 +126,30 @@ class KeyDashboardScreen(BaseScreen):
     # -----------------------------------------------------
     # DOOR MONITOR (START ONCE)
     # -----------------------------------------------------
-    def start_door_monitor(self):
-        if self._door_monitor_started:
-            return
+    # def start_door_monitor(self):
+    #     if self._door_monitor_started:
+    #         return
 
-        self._door_monitor_started = True
-        state = read_limit_switch(LIMIT_SWITCH)
-        self._last_door_state = state
+    #     self._door_monitor_started = True
+    #     state = read_limit_switch(LIMIT_SWITCH)
+    #     self._last_door_state = state
 
-        print("[DOOR] Door monitor started")
-        print("[DOOR] Initial:", "OPEN" if state == 1 else "CLOSED")
+    #     print("[DOOR] Door monitor started")
+    #     print("[DOOR] Initial:", "OPEN" if state == 1 else "CLOSED")
 
-    def monitor_door_status(self):
-        if not self._door_monitor_started:
-            return
+    # def monitor_door_status(self):
+    #     if not self._door_monitor_started:
+    #         return
 
-        state = read_limit_switch(LIMIT_SWITCH)
+    #     state = read_limit_switch(LIMIT_SWITCH)
 
-        if state != self._last_door_state:
-            if state == 1:
-                print("[DOOR] 🚪 OPEN")
-            else:
-                print("[DOOR] 🔒 CLOSED")
+    #     if state != self._last_door_state:
+    #         if state == 1:
+    #             print("[DOOR] 🚪 OPEN")
+    #         else:
+    #             print("[DOOR] 🔒 CLOSED")
 
-            self._last_door_state = state
+    #         self._last_door_state = state
 
     # -----------------------------------------------------
     # SCREEN EXIT
