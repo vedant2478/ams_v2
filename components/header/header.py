69c60caf29aaec05d82aa927bf2712e785b942ca
vedant2478 <vedant@csinc.in>
2026-01-05 18:25:49 +0530

    Log events

--- components/header/header.py
+++ components/header/header.py
@@ -1,12 +1,8 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
 
-from db import get_site_name   # global helper
 
 class Header(BoxLayout):
-    site_text = StringProperty("")
+    site_text = StringProperty("SITE")
     status_text = StringProperty("")
     time_text = StringProperty("")
-
-    def on_kv_post(self, base_widget):
-        self.site_text = get_site_name(session=base_widget.manager.db_session)
