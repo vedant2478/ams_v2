deeac43fdf5433cd29cba6447ef3de731a579b52
vedant2478 <vedant@csinc.in>
2026-01-09 14:45:00 +0530

    Going to done page

--- pages/activity_done/activity_done.py
+++ pages/activity_done/activity_done.py
@@ -1,30 +1,29 @@
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty, NumericProperty, ListProperty
 from kivy.clock import Clock
 from components.base_screen import BaseScreen
-
 
 class ActivityDoneScreen(BaseScreen):
     retrieved_text = StringProperty("")
     returned_text = StringProperty("")
     timestamp_text = StringProperty("")
     countdown = NumericProperty(5)
+    cards = ListProperty([])
 
-    def on_enter(self, *args):
-        self.countdown = 5
-        self._event = Clock.schedule_interval(self._tick, 1)
-
-    def on_leave(self, *args):
-        if hasattr(self, "_event"):
-            self._event.cancel()
-
-    def _tick(self, dt):
-        if self.countdown > 0:
-            self.countdown -= 1
-        if self.countdown == 0 and self.manager:
-            self.manager.current = "home"
-
-    def on_return_pressed(self):
-        if hasattr(self, "_event"):
-            self._event.cancel()
-        if self.manager:
-            self.manager.current = "home"
+    def on_pre_enter(self, *args):
+        self.cards = [
+            {
+                "key_name": "Master Key 1",
+                "taken_text": "2025-07-25 14:32:09",
+                "returned_text": "2025-07-25 14:59:09",
+            },
+            {
+                "key_name": "Master Key 1",
+                "taken_text": "2025-07-25 14:32:09",
+                "returned_text": "2025-07-25 14:59:09",
+            },
+            {
+                "key_name": "Master Key 1",
+                "taken_text": "2025-07-25 14:32:09",
+                "returned_text": "2025-07-25 14:59:09",
+            },
+        ]
